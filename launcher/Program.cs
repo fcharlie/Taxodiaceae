8ea91e0d54e8eb4762c01e786a6ae132fc96b6f1
Force Charlie <charlieio@outlook.com>
2016-06-20 20:42:49 +0800

    Add launcher

---
+++ launcher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Taxodiaceae
+{
+    class App
+    {
+        static int Main(string[] args)
+        {
+            var filename = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + "\\bin\\Taxodiaceae.exe";
+            //MessageBox.Show(filename);
+            if (!System.IO.File.Exists(filename))
+            {
+                MessageBox.Show(filename, "Not Found Taxodiaceae", MessageBoxButton.OK, MessageBoxImage.Error);
+                return -1;
+            }
+            Process ps = new Process();
+            ps.StartInfo.FileName = filename;
+            try
+            {
+                ps.Start();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Start Taxodiaceae Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return 0;
+        }
+    }
+}
